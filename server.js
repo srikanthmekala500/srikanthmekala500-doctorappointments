9d9ec925387a5ece6dd184f54348a142939f8966
srikanth mekala <125475567+srikanthmekala500@users.noreply.github.com>
2024-05-16 21:02:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.use(express.static(path.join(__dirname,"./build")))
 app.get("*",function(req,res){
 res.sendFile(path.join(__dirname,"./build/index.html"))
 })
-app.listen(PORT,()=>{
+app.listen(port,()=>{
     console.log(`server get started ${process.env.DEV_MODEL} ${port}`)
 })
